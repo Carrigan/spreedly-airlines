7c469bb4c91edf6f31fc5752df1b614107ba6cf1
Brian Carrigan <bcarrigan@spreedly.com>
2020-01-09 11:01:39 -0500

    Started a purchase form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
     end
+
+    def cart
+        @product = Product.find(params.require(:id))
+    end
+
+    def purchase
+        binding.pry
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
+
+  get "/products/:id/purchase", to: "products#cart", as: "product_cart"
+  post "/products/:id/purchase", to: "products#purchase", as: "product_purchase"
 end
